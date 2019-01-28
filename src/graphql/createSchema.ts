63765dd060a96f638a9c041ea52b781345e76837
Ramzan Chasygov <theliketurbo@gmail.com>
2019-01-29 01:50:15 +0300

    added authentication

--- src/graphql/createSchema.ts
+++ src/graphql/createSchema.ts
@@ -2,10 +2,18 @@ import { buildSchema } from 'type-graphql';
 import UserLoginResolver from '../modules/user/Login';
 import UserMeResolver from '../modules/user/Me';
 import UserRegisterResolver from '../modules/user/Register';
+import Context from '../types/context';
 
 const createSchema = async () =>
   buildSchema({
-    resolvers: [UserRegisterResolver, UserLoginResolver, UserMeResolver]
+    resolvers: [UserRegisterResolver, UserLoginResolver, UserMeResolver],
+    authChecker({ context: { req } }: { context: Context }) {
+      if (req.session.userId) {
+        return true;
+      }
+
+      return false;
+    }
   });
 
 export default createSchema;
