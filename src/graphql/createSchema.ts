7d4ae715ef3f76c0f8133fc6d02bd5ae8f4e9606
Ramzan Chasygov <theliketurbo@gmail.com>
2019-01-29 01:42:43 +0300

    implemented me resolver

--- src/graphql/createSchema.ts
+++ src/graphql/createSchema.ts
@@ -1,11 +1,11 @@
 import { buildSchema } from 'type-graphql';
-import JobResolver from '../modules/job/resolver';
 import UserLoginResolver from '../modules/user/Login';
+import UserMeResolver from '../modules/user/Me';
 import UserRegisterResolver from '../modules/user/Register';
 
 const createSchema = async () =>
   buildSchema({
-    resolvers: [JobResolver, UserRegisterResolver, UserLoginResolver]
+    resolvers: [UserRegisterResolver, UserLoginResolver, UserMeResolver]
   });
 
 export default createSchema;
---
+++ src/modules/user/Me.ts
@@ -0,0 +1,17 @@
+import { Ctx, Query, Resolver } from 'type-graphql';
+import User from '../../entities/User';
+import Context from '../../types/context';
+
+@Resolver(of => User)
+class UserMeResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req }: Context): Promise<User | null> {
+    if (!req.session.userId) {
+      return null;
+    }
+
+    return User.findOne(req.session.userId);
+  }
+}
+
+export default UserMeResolver;
