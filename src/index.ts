a16ee1e0aac63617bbcdcd01482958c3acf1b6e8
Ramzan Chasygov <theliketurbo@gmail.com>
2019-01-28 17:59:34 +0300

    updated dev settings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from 'apollo-server-express';
-import * as express from 'express';
+import express from 'express';
 import createSchema from './graphql/createSchema';
 import connectToDB from './middlewares/connectToDB';
 
