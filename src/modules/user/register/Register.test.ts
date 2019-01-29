0bdaadb3e74e5f5aa63048ae1c59f597136fb8c2
Ramzan Chasygov <theliketurbo@gmail.com>
2019-01-29 17:42:15 +0300

    added Register test

--- src/modules/user/register/Register.test.ts
+++ src/modules/user/register/Register.test.ts
@@ -1,4 +1,6 @@
+import faker from 'faker';
 import { Connection } from 'typeorm';
+import User from '../../../entities/User';
 import connectTestDB from '../../../tests/utils/connectTestDB';
 import graphQLCall from '../../../tests/utils/graphQLCall';
 
@@ -24,17 +26,36 @@ const registerMutation = `
 
 describe('Register', () => {
   it('create user', async () => {
-    const result = await graphQLCall({
+    const user = {
+      firstName: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    const response = await graphQLCall({
       source: registerMutation,
       variableValues: {
-        input: {
-          firstName: 'Oleg',
-          email: 'oleg@mail.ru',
-          password: 'SuperOleg'
+        input: user
+      }
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        register: {
+          id: expect.any(String),
+          email: user.email,
+          firstName: user.firstName
         }
       }
     });
 
-    console.log(result);
+    const dbUser = await User.findOne(response.data.register.id);
+    expect(dbUser).toMatchObject({
+      id: expect.any(Number),
+      firstName: user.firstName,
+      email: user.email,
+      password: expect.any(String),
+      confirmed: false
+    });
   });
 });
