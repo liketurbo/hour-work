c67d3f1cd9e24cb6fd1bac12017383900b3a80c5
Ramzan Chasygov <theliketurbo@gmail.com>
2019-02-03 21:45:12 +0300

    fixed docker-compose

--- packages/server/src/middlewares/connectDB.ts
+++ packages/server/src/middlewares/connectDB.ts
@@ -1,8 +1,23 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
+import ms from 'ms';
 
 const connectDB = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection(connectionOptions);
+
+  let retries = 5;
+  while (retries) {
+    try {
+      return createConnection(connectionOptions);
+    } catch (err) {
+      console.log(err);
+      retries -= 1;
+      console.log(`retries left: ${retries}`);
+      // wait 1, 2, 3, 4, 5 seconds
+      await new Promise(res => setTimeout(res, ms(`${5 - retries}s`)));
+    }
+  }
+
+  return null;
 };
 
 export default connectDB;
